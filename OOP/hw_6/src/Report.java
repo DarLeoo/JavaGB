db457512bcfd3957d022d03d7287895d99956f44
DarLeo <dangerrrleo@gmail.com>
2023-03-13 23:18:44 +0400

    add hw_6

---
+++ OOP/hw_6/src/Report.java
@@ -0,0 +1,9 @@
+public class Report {
+    private User user;
+    public Report (User user){
+        this.user = user;
+    }
+    public void printReport(){
+        System.out.println("Report for user: " + user.getName());
+    }
+}
